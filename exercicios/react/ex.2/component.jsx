7ef5cb0534816c37c00625d8977e4f9a2bcb18e4
Diego Parente <diego.parente@rddsolutions.com.br>
2019-02-07 22:29:25 -0200

    #3 *18-28 Exercicios de React

---
+++ exercicios/react/ex.2/component.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// export default function() {
+//     return <h1>Primeiro Componente!</h1>
+// }
+
+// export default () => {
+//     return <h1>Primeiro Componente!</h1>
+// }
+
+// export default () => <h1>Primeiro Componente!</h1>
+
+export default () => (
+    <h1>Primeiro Componente!</h1>
+);
