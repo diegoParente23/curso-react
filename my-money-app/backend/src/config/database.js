bedebf55e97adbb5387214eb97e74bfdbb23bff8
Diego Parente <diego.parente@outlook.com.br>
2019-09-18 22:11:05 -0300

    #8 *108 Fim do backend

--- my-money-app/backend/src/config/database.js
+++ my-money-app/backend/src/config/database.js
@@ -1,3 +1,8 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
-module.exports = mongoose.connect('mongodb://192.168.99.100/mymoney')
+module.exports = mongoose.connect('mongodb://192.168.99.100/mymoney', { userNewUrlParser: true })
+
+mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório"
+mongoose.Error.messages.Number.min = "O '{VALUE}' informado é menor que o limite mínimo de '{MIN}'."
+mongoose.Error.messages.Number.max = "O '{VALUE}' informado é menor que o limite máximo de '{MAX}'."
+mongoose.Error.messages.String.enum = "O '{VALUE}' não é válido para o atributo '{PATH}'."
