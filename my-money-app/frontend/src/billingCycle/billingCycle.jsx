7949b1f2f1fb29996f4b1f947ba881c09837560e
Diego Parente <diego.parente@outlook.com.br>
2019-10-01 21:29:40 -0300

    #9 *154 Excluir Ciclo de pagamento

--- my-money-app/frontend/src/billingCycle/billingCycle.jsx
+++ my-money-app/frontend/src/billingCycle/billingCycle.jsx
@@ -10,9 +10,10 @@ import TabsHeader from '../common/tab/tabsHeader'
 import TabsContent from '../common/tab/tabsContent'
 import TabContent from '../common/tab/tabContent'
 import TabHeader from '../common/tab/tabHeader'
-import { selectTab, showTabs } from '../common/tab/tabActions'
 import List from './billingCycleList'
 import Form from './billingCycleForm'
+import { selectTab, showTabs } from '../common/tab/tabActions'
+import { create, update } from './billingCycleActions'
 
 class BillingCycle extends Component {
 
@@ -38,9 +39,11 @@ class BillingCycle extends Component {
                                 <List />
                             </TabContent>
                             <TabContent id="tabCreate">
-                                <Form />
+                                <Form onSubmit={this.props.create} />
                             </TabContent>
-                            <TabContent id="tabUpdate"><h1>Alterar</h1></TabContent>
+                            <TabContent id="tabUpdate">
+                                <Form onSubmit={this.props.update} />
+                            </TabContent>
                             <TabContent id="tabDelete"><h1>Excluir</h1></TabContent>
                         </TabsContent>
                     </Tabs>
@@ -50,5 +53,5 @@ class BillingCycle extends Component {
     }
 }
 
-const mapDispatchToPros = dispatch => bindActionCreators({ selectTab, showTabs }, dispatch)
+const mapDispatchToPros = dispatch => bindActionCreators({ selectTab, showTabs, create, update }, dispatch)
 export default connect(null, mapDispatchToPros)(BillingCycle)
--- my-money-app/frontend/src/billingCycle/billingCycleForm.jsx
+++ my-money-app/frontend/src/billingCycle/billingCycleForm.jsx
@@ -1,18 +1,34 @@
 import React, { Component } from 'react'
+import { reduxForm, Field } from 'redux-form'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+
+import { init } from './billingCycleActions'
+import labelAndInput from '../common/form/labelAndInput'
 
 class BillingCycleForm extends Component {
     render() {
-        return (
-            <form role="form">
-                <div className="box-body">
+        const { handleSubmit } = this.props
 
+        return (
+            <form role="form" onSubmit={handleSubmit}>
+                <div className="box-body">
+                    <Field name='name' component={labelAndInput}
+                        label='Nome' cols='12 4' placeholder='Informe o nome' />
+                    <Field name='month' component={labelAndInput}
+                        label='Mês' cols='12 4' placeholder='Informe o mês' type='number' />
+                    <Field name='year' component={labelAndInput}
+                        label='Ano' cols='12 4' placeholder='Informe o ano' type='number' />
                 </div>
                 <div className="box-footer">
                     <button type="submit" className="btn btn-primary">Submit</button>
+                    <button type="button" className="btn btn-default" onClick={this.props.init}>Cancelar</button>
                 </div>
             </form>
         )
     }
 }
 
-export default BillingCycleForm
+BillingCycleForm = reduxForm({ form: 'billingCycleForm', destroyOnUnmount: false })(BillingCycleForm)
+const mapDispatchToProps = dispatch => bindActionCreators({ init }, dispatch)
+export default connect(null, mapDispatchToProps)(BillingCycleForm)
